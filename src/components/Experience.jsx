3409eb40c83f45fca199b517e58b28403f6827f0
NAME <mail@gmail.com>
2023-03-15 23:18:54 +0100

    Create Experience section for Portfolio

---
+++ src/components/Experience.jsx
@@ -0,0 +1,47 @@
+import { educationAndExperience } from "../data";
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
+import 'react-vertical-timeline-component/style.min.css'
+
+const Card = ({experience}) => {
+  return (
+    <div className="experienceCard">
+      <VerticalTimelineElement
+      contentStyle={{backgroundColor: 'purple', color: '#eee'}}
+      date={experience.year}
+      icon={
+        <div className="school">
+          <img src={experience.logo} alt="Logo"/>
+        </div>
+      } 
+      >
+        <h3>{experience.name}</h3>
+        <p>{experience.field}</p>
+        <p>{experience.role}</p>
+        <ul>
+          {
+            experience.highlights.map((point, index) => (
+              <li key={index}>{point}</li>
+          ))
+          }
+        </ul>
+      </VerticalTimelineElement>
+    </div>
+  )
+}
+
+const Experience = () => {
+  return (
+    <section id="education">
+      <h2>Education and Experience</h2>
+      <VerticalTimeline>
+        {
+          educationAndExperience.map((item, index) => (
+            <Card key={index} experience={item} />
+          ))
+        }
+      </VerticalTimeline>
+    </section>
+  )
+}
+
+export default Experience
