a7a43064934f82c3af365a467e487e78bdfbbc3e
NAME <mail@gmail.com>
2023-03-15 23:36:47 +0100

    Render and create route for the Portfolio Sections.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import ContactUs from './views/ContactUs'
+import OperatingSystem from './components/OperatingSystem';
+import Hero from './views/Hero';
+import Nav from './views/Nav';
+import Tech from './components/Tech';
+import Experience from './components/Experience';
+import Works from './components/Works';
+import About from './views/About';
+import Footer from './views/Footer';
 import './App.css'
 
 function App() {
   return (
     <div className="App">
+      <Nav />
+      <Hero />
+      <Tech />
+      <OperatingSystem />
+      <Experience />
+      <Works />
+      <About />
       <ContactUs />
+      <Footer />
     </div>
   );
 }
