d69203a2fec83f5d6ab88d68afb58259509c7ec9
NAME <mail@gmail.com>
2023-03-15 23:33:35 +0100

    Create the Space Component with stars.

--- src/components/scenes/Space.jsx
+++ src/components/scenes/Space.jsx
@@ -5,11 +5,11 @@ import * as random from 'maath/random/dist/maath-random.esm'
 
 const Stars = (props) => {
   const ref = useRef()
-  const sphere = random.inSphere(new Float32Array(6000), {radius: 1.2})
+  const sphere = random.inSphere(new Float32Array(10000), {radius: 1.2})
 
   useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 10
-    ref.current.rotation.y -= delta / 20
+    ref.current.rotation.x -= delta / 30
+    ref.current.rotation.y -= delta / 50
   })
 
   return (
@@ -17,10 +17,10 @@ const Stars = (props) => {
       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props} >
         <PointMaterial
         transparent
-        color="#f00"
-        size={.01}
+        color="#ddd"
+        size={.007}
         sizeAttentuation={true}
-        depthWrite={false}
+        depthWrite={true}
       />
       </Points>
     </group>
