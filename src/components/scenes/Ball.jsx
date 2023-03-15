9444f3d0345ce1135effc225efa9e33358a17a1e
NAME <mail@gmail.com>
2023-03-15 23:34:14 +0100

    Created thr Rotating ball component.

--- src/components/scenes/Ball.jsx
+++ src/components/scenes/Ball.jsx
@@ -1,9 +1,37 @@
-const Ball = () => {
-  return <h1>BAll</h1>
+import {Suspense} from 'react'
+import { Canvas } from '@react-three/fiber'
+import {Decal, OrbitControls, Preload, useTexture, Float } from '@react-three/drei'
+
+const Ball = (props) => {
+  const [logo] = useTexture([props.imgUrl])
+  return (
+    <>
+    <Float speed={1.5} rotationIntensity={1} floatIntensity={3}>
+      <ambientLight intensity={1} />
+      <directionalLight position={[0,0, 0.05]} />
+      <mesh receiveShadow castShadow scale={2.3}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshLambertMaterial color="#333" polygonOffset polygonOffsetFactor={-6} flatShading/>
+        <Decal flatShading position={[0, 0, 1]} rotation={[2*Math.PI, 0, 6]} map={logo} />
+      </mesh>
+    </Float>
+    </>
+  )
 }
 
-const BallScene = () => {
-  return <Ball />
+const BallScene = (props) => {
+  return (
+    <Canvas
+      frameloop='demand'
+      gl={{preserveDrawingBuffer: true}}
+  >
+      <Suspense fallback={false}>
+        <OrbitControls enableZoom={false}/>
+        <Ball imgUrl={props.imgUrl}/>
+      </Suspense>
+      <Preload/>
+    </Canvas>
+  )
 }
 
 export default BallScene
