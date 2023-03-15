5b128a1606aa06675210fdf33575dd64a338a433
NAME <mail@gmail.com>
2023-03-15 23:35:05 +0100

    Create the Contact Me section.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,8 +2,9 @@ const Contact = () => {
   return (
     <section id="contact">
       <form action="">
+        <h2>Contact Me</h2>
         <input type="email" placeholder="Enter your Email"/>
-        <textarea name="mail" id="message"></textarea>
+        <textarea name="mail" id="message" placeholder="Write your message"></textarea>
         <input type="submit" value="Send Message"/>
       </form>
     </section>
