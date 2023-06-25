724716e5a04e258e606326647ca36a3b828a4c8a
Solly <solobarine@gmail.com>
2023-06-25 23:02:46 +0100

    Change Technologies Display

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,42 +1,78 @@
-import Tilt from "react-parallax-tilt";
-import { motion, useAnimation } from "framer-motion";
+
 import { useInView } from "react-intersection-observer";
 import { useEffect } from "react";
-import { technologies } from "../data";
-import { stagger } from "../data/variants";
+import languages from "../data/languages";
+import front_end from "../data/front_end";
+import back_end from "../data/backend";
+import databases from "../data/databases"
+import test_tools from "../data/test_tools"
+import tools from "../data/tools"
+import TechSkill from "./TechSkill";
 
 const Tech = () => {
 
-  const controls = useAnimation()
-  const [ref, inView] = useInView()
-
-  useEffect(() => {
-    if(inView) {
-      controls.start('visible')
-    }
-  }, [controls, inView])
-
   return (
     <section id="technologies">
-      <h2>Technologies</h2>
-      <div id="tech">
+      <h2>Skills and Abilities</h2>
+      <div className="stack">
+        <h3>Languages</h3>
+        <div>
+          {
+            languages.map((item, index) => (
+              <TechSkill key={index} data={item} index={index} />
+            ))
+          }
+        </div>
+      </div>
+      <div className="stack">
+        <h3>Front End</h3>
+        <div>
+          {
+            front_end.map((item, index) => (
+              <TechSkill key={index} data={item} index={index} />
+            ))
+          }
+        </div>
+      </div>
+      <div className="stack">
+        <h3>Back End</h3>
+        <div>
         {
-          technologies.map((tech, index) => (
-            <motion.div
-            ref={ref}
-            variants={stagger(index)}
-            initial='hidden'
-            animate={controls}
-            className="tech" key={index}>
-              <Tilt>
-                <div id="techContent">
-                  <div className="techImg"><img src={tech.imgUrl} alt="technology"/></div>
-                  <h4>{tech.name}</h4>
-                </div>
-              </Tilt>
-            </motion.div>
-          ))
-        }
+            back_end.map((item, index) => (
+              <TechSkill key={index} data={item} index={index} />
+            ))
+          }
+        </div>
+      </div>
+      <div className="stack">
+        <h3>DataBases</h3>
+        <div>
+        {
+            databases.map((item, index) => (
+              <TechSkill key={index} data={item} index={index} />
+            ))
+          }
+        </div>
+      </div>
+      <div className="stack">
+        <h3>Testing Libraries</h3>
+        <div>
+        {
+            test_tools.map((item, index) => (
+              <TechSkill key={index} data={item} index={index} />
+            ))
+          }
+        </div>
+      </div>
+      <div className="stack">
+        <h3>Tools</h3>
+        <div>
+        {
+            tools.map((item, index) => (
+              <TechSkill key={index} data={item} index={index} />
+            ))
+          }
+        </div>
       </div>
     </section>
   )
