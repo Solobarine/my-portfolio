9737a5b48f45c17a7362b82984ad52cb11fb80b2
Solly <solobarine@gmail.com>
2023-11-10 19:48:16 +0100

    Update Infrastructure

--- src/data/infrastructure.js
+++ src/data/infrastructure.js
@@ -1,22 +1,26 @@
 export const infrastructure = [
-    {
-        name: 'Kubernetes',
-        imgUrl: '/technologies/kubernets.svg'
-    },
-    {
-        name: 'Azure DevOps',
-        imgUrl: '/technologies/azure.svg'
-    },
-    {
-        name: 'Gitlab',
-        imgUrl: '/technologies/gitlab.svg'
-    },
-    {
-        name: 'Github',
-        imgUrl: '/technologies/github.svg'
-    },
-    {
-        name: 'Docker',
-        imgUrl: '/technologies/docker.svg'
-    }
-]
+  {
+    name: 'Ubuntu',
+    imgUrl: '/technologies/os/ubuntu.png'
+  },
+  {
+    name: 'Git',
+    imgUrl: '/technologies/git.png'
+  },
+  {
+    name: 'Azure DevOps',
+    imgUrl: '/technologies/azure.svg'
+  },
+  {
+    name: 'Gitlab',
+    imgUrl: '/technologies/gitlab.svg'
+  },
+  {
+    name: 'Github',
+    imgUrl: '/technologies/github.svg'
+  },
+  {
+    name: 'Docker',
+    imgUrl: '/technologies/docker.svg'
+  }
+];
