ece65ccc1293815897954d02cbb8ae5492cef6b1
Solly <solobarine@gmail.com>
2023-11-10 23:25:36 +0100

    Add Download Button

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,10 +1,16 @@
-import { about } from "../data"
+import { about } from '../data'
 
 const About = () => {
   return (
-    <section id="about">
+    <section id='about'>
       <h2>About Me</h2>
       <p>{about.pitch}</p>
+      <a
+        className='resumeDownload'
+        href='/downloadables/Solomon_Resume.pdf'
+        download="solomon's resume">
+        Download Resume
+      </a>
     </section>
   )
 }
