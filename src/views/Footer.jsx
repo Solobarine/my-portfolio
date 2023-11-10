1591d28c7c717cb4fbd2cdd7edb4c047edfb17f0
Solly <solobarine@gmail.com>
2023-11-10 22:33:30 +0100

    Add Current Date Variable to Footer.

--- src/views/Footer.jsx
+++ src/views/Footer.jsx
@@ -1,24 +1,26 @@
-import { socials } from "../data"
+import { socials } from '../data';
 
 const Footer = () => {
+  const date = new Date();
   return (
     <footer>
-      <div className="socials">
-        {
-          socials.map((icon, index) => (
-            <a
+      <div className='socials'>
+        {socials.map((icon, index) => (
+          <a
             href={icon.url}
-            target="noreferrer"
-            rel="noreferrer"
+            target='noreferrer'
+            rel='noreferrer'
             key={index}>
-              <img src={icon.logo} alt={`${icon.name} handle`}/>
-            </a>
-          ))
-        }
+            <img
+              src={icon.logo}
+              alt={`${icon.name} handle`}
+            />
+          </a>
+        ))}
       </div>
-      <p>Copyright &copy; 2023. All rights reserved.</p>
+      <p>Copyright &copy; {date.getFullYear()}. All rights reserved.</p>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
