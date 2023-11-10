1e84588ea0b47a464d5a66c3b3482d935f9ea02e
Solly <solobarine@gmail.com>
2023-11-10 19:45:44 +0100

    Update Menu Component.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,21 +1,43 @@
-import { useRef } from "react"
+import { useRef } from 'react';
 
-const Menu = ({isOpen, setIsOpen}) => {
-  const ref = useRef()
+const Menu = ({ isOpen, setIsOpen }) => {
+  const ref = useRef();
 
   const toggle = () => {
-    ref.current.style.top = '-150px'
-    setIsOpen(!isOpen)
-  }
+    // ref.current.style.top = '-200px';
+    setIsOpen(!isOpen);
+  };
   return (
-    <div ref={ref} id="menu">
-      <a href="#landing" onClick={toggle}>Home</a>
-      <a href="#about" onClick={toggle}>About Me</a>
-      <a href="#education" onClick={toggle}>Education</a>
-      <a href="#works" onClick={toggle}>Projects</a>
-      <a href="#contactMe" onClick={toggle}>Contact Me</a>
+    <div
+      ref={ref}
+      id='menu'>
+      <a
+        href='#landing'
+        onClick={toggle}>
+        Home
+      </a>
+      <a
+        href='#about'
+        onClick={toggle}>
+        About Me
+      </a>
+      <a
+        href='#education'
+        onClick={toggle}>
+        Education
+      </a>
+      <a
+        href='#works'
+        onClick={toggle}>
+        Projects
+      </a>
+      <a
+        href='#contactMe'
+        onClick={toggle}>
+        Contact Me
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
